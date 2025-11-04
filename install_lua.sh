4c410e353c8a1d5028c1acf9a0fec5ac30f2c6ea
csecsoliver <0oliogamer0@gmail.com>
2025-11-04 08:21:48 +0000

    trying to make lua work

---
+++ install_lua.sh
@@ -0,0 +1,6 @@
+curl -o redbean.com https://redbean.dev/redbean-2.2.com
+curl -o unzip.com https://redbean.dev/unzip.com
+curl -o zip.com https://redbean.dev/zip.com
+curl -o sqlite3.com https://redbean.dev/sqlite3.com
+curl -o ./.lua/fullmoon.lua https://github.com/csecsoliver/fullmoon-fork/raw/refs/heads/master/fullmoon.lua
+chmod +x ./*.com
