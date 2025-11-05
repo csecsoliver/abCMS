5ccbe1f94517dc15020a509be0f9e3bd76519b76
Csécs Olivér Elemér <csecs.oliver.elemer@students.jedlik.eu>
2025-11-05 13:15:45 +0000

    lua

--- .init.lua
+++ .init.lua
@@ -1,5 +1,14 @@
 local fm = require("fullmoon")
-fm.setRoute("/", function ()
-    return "hello world"
+local posts = require("posts")
+local pages = require("pages")
+
+
+fm.setTemplate({"/views/", tmpl = "fmt"})
+
+
+---@param r table
+---@return function
+fm.setRoute("/", function (r)
+    return fm.serveContent()
 end)
 fm.run()
